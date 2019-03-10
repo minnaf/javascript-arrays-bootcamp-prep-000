6cadc90e8959d2f33e720cbbec9ce5a08a2ba0ef
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-10 17:02:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,4 +15,7 @@ function addElementToEndOfArray(array, element){
   return [...array,element];
 }
 
-
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
