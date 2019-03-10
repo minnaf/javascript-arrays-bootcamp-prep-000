a4e5459b0b39344265b91ceb3b8b15aa9182882e
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-10 17:12:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,5 +39,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0-arrays.length-1)
+  return array.slice(0-array.length-1)
 }
